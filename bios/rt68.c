6f84713bbfda18b53257e28dde66fcbdaa919c88
Michele Fabbri <fabbrimichele75@gmail.com>
2024-02-21 18:11:02 +0100

    Working debug on serial. Panic: Bus error

--- bios/rt68.c
+++ bios/rt68.c
@@ -1,22 +1,41 @@
 #include "emutos.h"
 #include "rt68.h"
 
+#define SERDATR     *(volatile UBYTE*)0x37e801
+#define SERDAT      *(volatile UBYTE*)0x37e803
+#define SERDAT_TBE  4 /* bit2: Transmit Buffer */
 
 // TODO: https://github.com/emutos/emutos/blob/9c42ef0f2c2fa9d1e217fced9da68901b2326e8b/bios/dana.c
 
-/* Moved to rt68asm.S
+
+BOOL rt68_rs232_initialized = FALSE;
+
+extern void rt68_init(void) {
+    // TODO: temporary to see as earlier as 
+    //       possible the debug messages
+    rt68_68681_init();
+    rt68_rs232_initialized = TRUE;
+}
+
 void rt68_rs232_init(void) 
 { 
-
+    // TODO: I should probably reimplement the intialization
+    //       from ASM to C, it's not time critical
 }
-*/
 
 BOOL rt68_rs232_can_write(void) 
 { 
-    return 0;
+    if (rt68_rs232_initialized == FALSE)
+        return FALSE;
+
+    return SERDATR & SERDAT_TBE;
 }
 
 void rt68_rs232_writeb(UBYTE b) 
 { 
-
+    if (rt68_rs232_initialized == TRUE) 
+    {
+        while (!rt68_rs232_can_write()) {}
+        SERDAT = b;
+    }
 }
--- bios/rt68.h
+++ bios/rt68.h
@@ -9,6 +9,9 @@ extern void rt68_rs232_init(void);
 extern BOOL rt68_rs232_can_write(void);
 extern void rt68_rs232_writeb(UBYTE b);
 
+// _rt68_68681_init is for driver internal use 
+extern void rt68_68681_init(void);
+
 #endif /* MACHINE_RT68 */
 
 #endif /* DANA_RT68 */
